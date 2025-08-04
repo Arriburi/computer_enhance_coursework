b1100068330cf1737b0dd245cef19a51ea6b75c1
Arriburi <luca.arrighetti@gmail.com>
2025-08-04 16:06:03 +0200

    Completed Register/memory to/from register function. 'Switch' changed with 'if' coinditions for recognizing specific opcodes for MOV

--- decoder.py
+++ decoder.py
@@ -13,70 +13,56 @@ class Opcode(Enum):
     MOV = 0b100010
 
 reg8bit = ['AL', 'CL', 'DL', 'BL', 'AH', 'CH', 'DH', 'BH'] ## if W = 0
-
 reg16bit = ['AX', 'CX', 'DX', 'BX', 'SP', 'BP', 'SI', 'DI'] ## if W = 1
+effective_addr = ['BX + SI', 'BX + DI', 'BP + SI', 'BP + DI', 'SI', 'DI', 'BP', 'BX']
+
 
 #def reg_to_reg(mode:int, reg:int, rm:int):
 #    bytes_consumed = 0
 #    return bytes_consumed
     
-def memreg_to_reg(file:bytes, index : int, word:int):
+def process_mov_instruction(file:bytes, index : int, word:int, direction: int):
     bytes_consumed = 1
     second_byte = file[index + 1]
     bytes_consumed += 1
+
+    third_byte = file[index + 2]
+    fourth_byte = file[index + 3]
+    displacement = third_byte | (fourth_byte << 8)
 
     mode = (second_byte >> 6) & 0b11
     reg = (second_byte >> 3) & 0b111  
     rm = second_byte & 0b111 
 
+    reg_operand = reg16bit[reg] if word else reg8bit[reg]
+
     match mode:
-        case 0b00:
-            print("No displacement except if R/M = 110 then 16-bit displacement")
-        case 0b01:
-            print("Memory Mode, 8-bit")
-        case 0b10:
-            print("Memory mode, 16-bit")
-        case 0b11:
-            print("Register Mode, no displacement")
-            if word:
-                source = reg16bit[reg]
-                destination = reg16bit[rm]
+        case 0b00: # No displacement except if R/M = 110 then 16-bit displacement
+            if rm == 0b110:  
+                rm_operand = f"[{displacement}]"
+                bytes_consumed += 2
             else:
-                source = reg8bit[reg]
-                destination = reg8bit[rm]
+                rm_operand = f"[{effective_addr[rm]}]"
+        case 0b01: #Memory Mode, 8-bit
+            rm_operand = f"[{effective_addr[rm]} + {third_byte}]"
+            bytes_consumed += 1
+        case 0b10: # Memory Mode, 16-bit
+            rm_operand = f"[{effective_addr[rm]} + {displacement}]"
+            bytes_consumed += 2
+        case 0b11: # Register Mode, no displacement
+            rm_operand = reg16bit[rm] if word else reg8bit[rm]
         case _: 
             print("something went wrong")
 
-    return  source, destination, bytes_consumed
-
-def reg_to_memreg(file:bytes, index : int, word:int):
-    bytes_consumed = 1
-    second_byte = file[index + 1]
-    bytes_consumed += 1
-
-    mode = (second_byte >> 6) & 0b11
-    reg = (second_byte >> 3) & 0b111  
-    rm = second_byte & 0b111 
-
-    match mode:
-        case 0b00:
-            print("No displacement except if R/M = 110 then 16-bit displacement")
-        case 0b01:
-            print("Memory Mode, 8-bit")
-        case 0b10:
-            print("Memory mode, 16-bit")
-        case 0b11:
-            print("Register Mode, no displacement")
-            if word:
-                source = reg16bit[rm] 
-                destination = reg16bit[reg]
-            else:
-                source = reg8bit[rm]
-                destination = reg8bit[reg]
-        case _: 
-            print("something went wrong")
-
-    return  source, destination, bytes_consumed
+    if direction == 1:              # Memory/Register to Register
+        source = rm_operand
+        destination = reg_operand
+    else:                           # Register to Memory/Register
+        source = reg_operand
+        destination = rm_operand
+    
+    
+    return source, destination, bytes_consumed
 
 
 def parser(file_name: str):
@@ -84,29 +70,26 @@ def parser(file_name: str):
         file = f.read()
     index = 0 
     while index < len(file):
-        chunk = file[index] 
+        chunk = file[index]
         opcode = (chunk >> 2) & 0b111111      
         direction = (chunk >> 1) & 0b1 
         word = chunk & 0b1 
-        print(f"Byte value: {chunk}, Binary: {chunk:08b}")
+        print(f"Binary: {chunk:08b}")
 
-        match opcode, direction:
-            case 0b100010, 1:
-                print("MOV Memory/Register to Register")
-                source, destination, bytes_consumed = memreg_to_reg(file, index, word)
+        if (chunk & 0b11111100) == 0b10001000: ## 100010
+                print("matched MOV")
+                source, destination, bytes_consumed = process_mov_instruction(file, index, word, direction)
                 index += bytes_consumed
-            case 0b100010, 0:
-                print("MOV Register to Memory/Register")
-                source, destination, bytes_consumed = reg_to_memreg(file, index, word)
-                index += bytes_consumed
-            case 0b1100011, _:
-                print("MOV immediate to register/memory")
-                index += 1 
-            case 0b1011, _:
-                print("MOV immediate to register")
-                index += 1  
-            case _:
-                break
+        elif (chunk & 0b11111110) == 0b11000110:    ## 1100011x  
+            print("MOV immediate to reg/mem")
+            break
+        elif (chunk & 0b11110000) == 0b10110000:    ## 1011xxxx
+            print("MOV immediate to reg")
+            break
+        else:
+            print("Unknown instruction")
+            break
+
         print(Opcode(opcode).name, source, destination)
     return 1
 
@@ -116,5 +99,5 @@ if __name__=="__main__":
     listing37 = 'listing_0037_single_register_mov'
     listing38 = 'listing_0038_many_register_mov'
     listing39 = 'listing_0039_more_movs'
-    file_name = listing38
+    file_name = listing39
     parser(file_name)
